923e45ef282a3b5a3d3e6086faeff55b9f52a2ad
vineetap15 <vineetapandey24194@gmail.com>
2019-09-27 18:39:00 +0530

    adding linklist reverse program

--- Practisejava/src/app/LinkedListReverse.java
+++ Practisejava/src/app/LinkedListReverse.java
@@ -2,39 +2,93 @@ package app;
 
 import java.util.Scanner;
 
+class Node{
+     int data;
+     Node next;
+
+    Node(int d){
+        data =d;
+        next =null;
+    }
+    public int getData(){
+        return data;
+    }
+    
+    public Node getNextNode(){
+        return next;
+    }
+}
+
 public class LinkedListReverse {
     Node head;
+    int size;
 
-    static class Node{
-        int data;
-        Node next;
+    public void getList(LinkedListReverse l){
+        Node current = l.head;
+        System.out.println("Size-- " +l.size);
+        if(isEmpty()== true){
+            System.out.println("List is empty");
+        }
+        else{
+            System.out.println("linked list data : ");
+            while(current != null){
+                System.out.print(" " +current.data + " ");
+                current = current.next;
+            }
 
-        Node(int d){
-            data =d;
-            next =null;
+        } 
+    }
+
+    public boolean isEmpty(){
+        return head==null;
+    }
+
+    public void insertNode(LinkedListReverse l, int n){
+        Node newNode = new Node(n);
+        if(isEmpty()== true){
+            System.out.println("inserting-- " +n);
+            l.head = newNode;
+            size++;
+        }
+        else{
+            Node current = l.head;
+            while(current.next!=null){
+                
+                current = current.next;
+                
+            }
+            System.out.println("inserting-- " +n);
+            current.next = newNode;
+            size++;
         }
     }
 
+    public void deleteNode(int n){
+        
+    }
 
-    public static void reverse(Node node) {
+
+    public  void reverse(LinkedListReverse l) {
+        Node prev =null;
+        Node current = l.head;
+        Node next = null;
+        while (current != null) {
+			next = current.next;
+			current.next = prev;
+			prev = current;
+			current = next;
+		}
+		l.head = prev;
         
     }
 
     public static void main(String[] args) {
         LinkedListReverse l = new LinkedListReverse();
-        int num=0;  
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter no. of element you want to enter: ");
-        num = s.nextInt();
-        int arr[] = new int[num];
-        System.out.println("Enter the array " +num+ " items: ");
-        for(int i=0;i<num;i++){
-            
-            arr[i] = s.nextInt();
-            add(l, arr[i]);
-            
-        }
-
-      
-    }
+        l.insertNode(l,3);
+        l.insertNode(l,4);
+        l.insertNode(l,1);
+        l.getList(l);
+        l.reverse(l);
+        l.getList(l);
+}
 }
